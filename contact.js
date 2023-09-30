a08c232ed609ddb8563eaee8a03afe3e47793941
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-09-30 06:53:00 +0530

    contact details update
    a feedback method was applied using emailjs

--- contact.js
+++ contact.js
@@ -1,35 +1,11 @@
-// Initialize EmailJS with your User ID
-emailjs.init("c6fMK7cKe2DOpGncK");
+function SendMail(){
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    }
 
-// Function to send an email when the form is submitted
-function sendEmail(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-
-    // Get form data
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    // Define template parameters
-    const templateParams = {
-        to_name: "Recipient's Name", // Replace with the recipient's name or use a variable if you have it
-        from_name: name, // Use the name entered in the form
-        message: message, // Use the message entered in the form
-    };
-
-    // Send the email using EmailJS
-    emailjs.send("service_rrqigwi", "template_j0017sv", templateParams)
-        .then(function(response) {
-            console.log("Email sent successfully:", response);
-            // Optionally, you can reset the form here
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-        })
-        .catch(function(error) {
-            console.error("Email sending failed:", error);
-        });
+    emailjs.send("service_rrqigwi", "template_j0017sv", params).then(function(res){
+        alert("Success! " + res.status);
+    })
 }
-
-// Attach the sendEmail function to the form's submit event
-document.getElementById("btnSubmit").addEventListener("click", sendEmail);
