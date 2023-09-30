75bf549564b1a55ad63b23299e7ea8e26b1b66a5
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-09-30 08:10:36 +0530

    contact email update

--- contact.js
+++ contact.js
@@ -1,11 +1,21 @@
 function SendMail(){
     var params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value,
+        name : document.getElementById("full-name").value,
+        email : document.getElementById("email-id").value,
+        message : document.getElementById("message-feedback").value,
     }
 
     emailjs.send("service_rrqigwi", "template_j0017sv", params).then(function(res){
-        alert("Thank you for the feedback!" + res.status);
-    })
+        var thankYouMessage = document.getElementById("thank-you-message");
+            thankYouMessage.style.display = "block";
+
+        document.getElementById("full-name").value = "";
+        document.getElementById("email-id").value = "";
+        document.getElementById("message-feedback").value = "";
+
+        onsole.log("Email sent successfully:", res);
+        })
+        .catch(function (error) {
+            console.error("Email sending failed:", error);
+        });
 }
